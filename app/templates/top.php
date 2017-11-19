9cef36bc15e883581151241ab758069c2467da6d
Taran Pierce <taran.pierce@endurance.com>
2017-11-18 17:33:00 -0700

    master
    added google analytics

--- app/templates/top.php
+++ app/templates/top.php
@@ -6,10 +6,22 @@
     <meta name="description" content="Xbox Live API">
     <meta name="author" content="Taran Pierce">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-5FVJP3S');</script>
+    <!-- End Google Tag Manager -->
+
     <link rel="stylesheet" href="/css/base.css">
     <!--[if lt IE 9]>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
     <![endif]-->
   </head>
   <body>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5FVJP3S" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
     <?php include 'templates/nav-main.php' ?>
