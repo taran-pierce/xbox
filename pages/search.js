2b61f22278411353548ddf9f0dd363a5368f359e
Caleb Masters <caleb.masters@endurance.com>
2019-02-13 11:42:48 -0800

    Resetting API URI

--- pages/search.js
+++ pages/search.js
@@ -33,7 +33,7 @@ const Page = withRouter( (props) => (
 
 Page.getInitialProps = async function (context) {
   const apiHeaders = {
-    'X-AUTH': '3464bec5b15e8eba49519b9378bb3959cd0b79af',
+    'X-AUTH': '1aa66306d38baca0e5ded32ab8ff437c8a72bcb5',
     'Cache-Control': 'no-cache',
     'Access-Control-Allow-Origin': '*',
     'Content-Type': 'application/json'
