ed7ee6d0b3d6b165c6e7b78a3ca31618415a70c7
thecaleblee <calebmasters@Gmail.com>
2019-02-13 14:20:06 -0800

    Feature/user card updates (#2)
    * Updated activity card display style
    
    * Removed unused CSS & continued styling
    
    * Updated the achievement tag to be more responsive
    
    * Updated display type for achievements
    
    * Resetting API URI
    
    * Added new layout for cards
    
    * Removed unused classes
    
    * Fixed responsive view for mobile
    
    * Actualy fixed responsive :|
    
    * Updated User card sections styles

--- components/UserCard.js
+++ components/UserCard.js
@@ -1,15 +1,77 @@
 const UserCard = (props) => (
   <div>
     <style jsx>{`
+      .user-details {
+        position: fixed;
+        width: 15%;
+      }
+
       img {
         max-width: 100%;
+        border-radius: 12px 12px 0 0;
+        margin-bottom: -4px;
+      }
+
+      h4 {
+        background: #01770C;
+        border-radius: 0 0 12px 12px;
+        color: #fff;
+        margin: 0;
+        padding: 10px;
+        font-family: sans-serif;
+        font-weight: normal;
+        display: grid;
+        grid-template-columns: repeat(2, 1fr); 
+      }
+
+      .left {
+        color: #154619;
+        margin: 0;
+        font-family: sans-serif;
+        font-size: 12px;
+        align-self: center;
+      }
+
+      .right {
+        color: #154619;
+        margin: 0;
+        padding: 4px 0;
+        font-family: sans-serif;
+        font-size: 12px;
+        justify-self: end;
+      }
+
+      h4 p {
+        margin: 0;
+        padding: 0;
+        font-size: 12px;
+        justify-self: right;
+        top: -2px;
+        position: relative;
+      }
+
+      h4 p span {
+        display: inline-block;
+        background: rgba(255,255,255,0.8);
+        border-radius: 100px;
+        color: #01770C;
+        font-size: 12px;
+        font-family: sans-serif;
+        font-weight: bold;
+        padding: 5px 7px 3px;
+        margin-right: 5px;
       }
     `}</style>
-    <h4>Gamer Tag: {props.data.AppDisplayName}</h4>
-    <p>Gamer Score: {props.data.Gamerscore}</p>
-    <p>Tenure: {props.data.TenureLevel}</p>
-    <p>Rep: {props.data.XboxOneRep}</p>
-    <img src={props.data.GameDisplayPicRaw} />
+    <div className="user-details">
+      <img src={props.data.GameDisplayPicRaw} />
+      <h4>
+        {props.data.AppDisplayName}
+        <p><span>G</span>{props.data.Gamerscore}</p>
+        <span className="left">{props.data.TenureLevel} years</span>
+        <span className="right">{props.data.XboxOneRep}</span>
+      </h4>
+    </div>
+
   </div>
 )
 
