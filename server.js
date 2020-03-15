ab833d0a4c625037dd8886f831bb675fe12a73e8
Taran Pierce <taran.pierce@endurance.com>
2020-03-15 10:47:20 -0400

    master
    added custom server

---
+++ server.js
@@ -0,0 +1,29 @@
+const { createServer } = require('http')
+const { parse } = require('url')
+const next = require('next')
+
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+
+app.prepare().then(() => {
+  createServer((req, res) => {
+    // Be sure to pass `true` as the second argument to `url.parse`.
+    // This tells it to parse the query portion of the URL.
+    const parsedUrl = parse(req.url, true)
+    const { pathname, query } = parsedUrl
+
+    // example of looking for specific paths
+    if (pathname === '/a') {
+      app.render(req, res, '/b', query)
+    } else if (pathname === '/b') {
+      app.render(req, res, '/a', query)
+    } else {
+      // handles all other requests
+      handle(req, res, parsedUrl)
+    }
+  }).listen(process.env.PORT || 3000, err => {
+    if (err) throw err
+    console.log(`> Ready on http://localhost:${process.env.PORT || 3000}`)
+  })
+})
